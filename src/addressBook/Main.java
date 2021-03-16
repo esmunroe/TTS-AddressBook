d25e627797469b9642b9cd551dff857886e9d7d4
Elijah Munroe <elimunroe15@hotmail.com>
2021-03-16 09:57:40 -0400

    Address Book modification methods

--- src/addressBook/Main.java
+++ src/addressBook/Main.java
@@ -1,8 +1,32 @@
 package addressBook;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    private List<Entry> addressBook = new ArrayList<>();
+
+    public String addEntry(Entry entry){
+
+    }
+
+    public String removeEntry(String email){
+
+    }
+
+    public List<Entry> searchEntry(int searchType, String searchQuery){
+
+    }
+
+    public void printBook(){
+
+    }
+
+    public void deleteBook(){
+
+    }
 
     public static void main(String[] args) {
-	    
+
     }
 }
