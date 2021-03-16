1fede3938f6df9f81a6c0d9879a558ff4707ba8f
Elijah Munroe <elimunroe15@hotmail.com>
2021-03-16 10:23:12 -0400

    Add entry method

--- src/addressBook/Main.java
+++ src/addressBook/Main.java
@@ -7,7 +7,11 @@ public class Main {
     private List<Entry> addressBook = new ArrayList<>();
 
     public String addEntry(Entry entry){
-
+        if (addressBook.contains(entry)){
+            return "Error! This entry contains an email address already in the book.";
+        }
+        addressBook.add(entry);
+        return entry.toString() + "\nAdded new entry!";
     }
 
     public String removeEntry(String email){
